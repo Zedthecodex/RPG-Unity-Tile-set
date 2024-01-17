47c0b7e29f7c17957a3a0e134c90e9898d8bcebe
Zedthecodex <phalpichpissidh@gmail.com>
2024-01-17 23:42:06 +0700

    Starting Enemy State Machine

---
+++ Assets/Enemy.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Rigidbody2D rb { get; private set; }
+    public Animator anim { get; private set; }
+
+    public EnemyStateMachine stateMachine { get; private set; }
+
+    private void Awake()
+    {
+        
+    }
+}
