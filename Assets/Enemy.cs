84079b6995927705f67540a694bdc870e5316099
Zedthecodex <phalpichpissidh@gmail.com>
2024-01-20 14:04:14 +0700

    Finalizing enemy attack state

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -8,6 +8,8 @@ public class Enemy : Entity
     [Header("Move Info")]
     public float moveSpeed;
     public float idleTime;
+    public float battleTime;
+
     [Header("Attack Info")]
     public float attackDistance;
     public float attackCooldown;
--- Assets/SkeletonGroundedState.cs
+++ Assets/SkeletonGroundedState.cs
@@ -6,6 +6,7 @@ public class SkeletonGroundedState :EnemyState
 {
 
     protected Enemy_Skeleton enemy;
+    protected Transform player;
     public SkeletonGroundedState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Skeleton enemy) : base(_enemyBase, _stateMachine, _animBoolName)
     {
         this.enemy = enemy;
@@ -14,6 +15,8 @@ public class SkeletonGroundedState :EnemyState
     public override void Enter()
     {
         base.Enter();
+
+        player = GameObject.Find("Player").transform;
     }
 
     public override void Exit()
@@ -25,7 +28,7 @@ public class SkeletonGroundedState :EnemyState
     {
         base.Update();
 
-        if (enemy.IsPlayerDetected())
+        if (enemy.IsPlayerDetected() || Vector2.Distance(enemy.transform.position , player.transform.position) < 2)
             stateMachine.ChangeState(enemy.battleState);
     }
 
