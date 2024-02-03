a7e48288e2b88418af9462bff4d8409baa87784c
zedthecodex <phalpichpissidh@gmail.com>
2024-02-03 23:50:08 +0700

    Finalizing character UI and further improvements

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -1,32 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class UI : MonoBehaviour
 {
     [SerializeField] private GameObject characterUI;
+    [SerializeField] private GameObject skillTreeUI;
+    [SerializeField] private GameObject craftUI;
+    [SerializeField] private GameObject optionsUI;
 
     public UI_ItemTooltip itemToolTip;
+    public UI_StatToolTip statToolTip;
     void Start()
     {
+        SwitchTo(null);
+
+        itemToolTip.gameObject.SetActive(false);
+        statToolTip.gameObject.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+            SwitchWithKeyTo(characterUI);
+
+        if (Input.GetKeyDown(KeyCode.B))
+            SwitchWithKeyTo(craftUI);
+
+        if (Input.GetKeyDown(KeyCode.K))
+            SwitchWithKeyTo(skillTreeUI);
+
+        if (Input.GetKeyDown(KeyCode.O))
+            SwitchWithKeyTo(optionsUI);
     }
 
     public void SwitchTo(GameObject _menu)
     {
-        for(int i =0; i < transform.childCount; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(false);
 
         }
 
-        if(_menu != null)
+        if (_menu != null)
             _menu.SetActive(true);
+    }
+
+    public void SwitchWithKeyTo(GameObject _menu)
+    {
+        if (_menu != null && _menu.activeSelf)
+        {
+            _menu.SetActive(false);
+            return;
+        }
+
+        SwitchTo(_menu);
     }
 }
