56b2872a62794e565f3612948c07c2042ca8c543
Zedthecodex <phalpichpissidh@gmail.com>
2024-02-04 14:01:23 +0700

    Created Crafting UI - And additional fixes

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -9,6 +9,7 @@ public class UI : MonoBehaviour
 
     public UI_ItemTooltip itemToolTip;
     public UI_StatToolTip statToolTip;
+    public UI_CraftWindow craftWindow;
     void Start()
     {
         SwitchTo(null);
--- Assets/Scripts/UI/UI_CraftSlot.cs
+++ Assets/Scripts/UI/UI_CraftSlot.cs
@@ -1,20 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class UI_CraftSlot : UI_ItemSlot
 {
-    private void OnEnable()
+
+    protected override void Start()
     {
-        UpdateSlot(item);
+        base.Start();
+
+
+    }
+   public void SetupCraftSlot(ItemData_Equipment _data)
+    {
+        if (_data == null)
+            return;
+
+        item.data= _data;
+
+        itemImage.sprite = _data.icon;
+        itemText.text = _data.itemName;
+
+        if (itemText.text.Length > 12)
+            itemText.fontSize = itemText.fontSize * 0.8f;
+        else
+            itemText.fontSize = 24;
     }
 
     public override void OnPointerDown(PointerEventData eventData)
     {
-        ItemData_Equipment craftData = item.data as ItemData_Equipment;
+        //ItemData_Equipment craftData = item.data as ItemData_Equipment;
 
-        Inventory.instance.CanCraft(craftData, craftData.craftingMaterials);
+        //Inventory.instance.CanCraft(craftData, craftData.craftingMaterials);
+
+        ui.craftWindow.SetupCraftWindow(item.data as ItemData_Equipment);
     }
 
 }
