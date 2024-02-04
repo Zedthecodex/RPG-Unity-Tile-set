7a36e353fd82cb16b9d1c39d0646a8529a23603f
Zedthecodex <phalpichpissidh@gmail.com>
2024-02-04 22:04:48 +0700

    Create Skill tree UI

---
+++ Assets/Scripts/UI/UI_SkillToolTip.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UI_SkillToolTip : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI skillText;
+    [SerializeField] private TextMeshProUGUI skillName;
+
+    // Start is called before the first frame update
+    
+    public void ShowToolTip(string _skillDescription, string _skillName)
+    {
+        skillName.text = _skillName;
+        skillText.text = _skillDescription;
+        gameObject.SetActive(true);
+    }
+
+    public void HideToolTip() => gameObject.SetActive(false);
+}
